6b206f383b5444fe75d6a6153b5f763cf2f93870
safrin <safreenakokkadan@gmail.com>
2024-06-14 21:11:53 +0200

    difference of squares

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,19 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{n}")
+    let sum = n * (n + 1) / 2;
+    sum * sum
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{n}")
+    n * (n + 1) * (2 * n + 1) / 6
+}
+pub fn difference(n: u32) -> u32 {
+    let square_sum = square_of_sum(n);
+    let sum_squares = sum_of_squares(n);
+    square_sum - sum_squares
 }
 
-pub fn difference(n: u32) -> u32 {
-    unimplemented!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
-}
+
+
+
+
+
